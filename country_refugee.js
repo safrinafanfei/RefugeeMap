7135a56e26983b161f36b4a1588461067ba988f0
SAFRINAFANFEI <FEIF1021@GMAIL.COM>
2016-02-05 14:29:21 -0800

    geocoded country latlong

---
+++ country_refugee.js
@@ -0,0 +1,350 @@
+var arcdata = [{
+    sourceLocation: [-112.0737951, 33.44818315],
+    targetLocation: [13.3833, 52.5167],
+    city: "Phoenix",
+    team: "Arizona Cardinals",
+    index: 19.63042799,
+    rank: 22,
+    gradient: [{
+        offset: "30%",
+        color: "#97233F"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-78.8494, 42.9047],
+    targetLocation: [13.3833, 52.5167],
+    city: "Buffalo",
+    team: "Buffalo Bills",
+    index: 20.64914521,
+    rank: 20,
+    gradient: [{
+        offset: "90%",
+        color: "#00338D"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-87.6847, 41.8369],
+    targetLocation: [13.3833, 52.5167],
+    city: "Chicago",
+    team: "Chicago Bears",
+    index: 26.16048097,
+    rank: 16,
+    gradient: [{
+        offset: "95%",
+        color: "#0B162A"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-81.6697, 41.4822],
+    targetLocation: [13.3833, 52.5167],
+    city: "Cleveland",
+    team: "Cleveland Browns",
+    index: 31.00451806,
+    rank: 15,
+    gradient: [{
+        offset: "90%",
+        color: "#22150C"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-96.79711092, 32.7767],
+    targetLocation: [13.3833, 52.5167],
+    city: "Dallas",
+    team: "Dallas Cowboys",
+    index: 32.09848625,
+    rank: 13,
+    gradient: [{
+        offset: "50%",
+        color: "#002244"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-83.0458, 42.3314],
+    targetLocation: [13.3833, 52.5167],
+    city: "Detroit",
+    team: "Detroit Lions",
+    index: 38.28665857,
+    rank: 11,
+    gradient: [{
+        offset: "60%",
+        color: "#005A8B"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-88.0158, 44.5133],
+    targetLocation: [13.3833, 52.5167],
+    city: "Green Bay",
+    team: "Green Bay Packers",
+    index: 13.53537866,
+    rank: 27,
+    gradient: [{
+        offset: "60%",
+        color: "#203731"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-95.3698, 29.7604],
+    targetLocation: [13.3833, 52.5167],
+    city: "Houston",
+    team: "Houston Texans",
+    index: 14.56351336,
+    rank: 25,
+    gradient: [{
+        offset: "50%",
+        color: "#A71930"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-86.1480, 39.7910],
+    targetLocation: [13.3833, 52.5167],
+    city: "Indianapolis",
+    team: "Indianapolis Colts",
+    index: 13.87407149,
+    rank: 26,
+    gradient: [{
+        offset: "40%",
+        color: "#002C5F"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-81.6614, 30.3369],
+    targetLocation: [13.3833, 52.5167],
+    city: "Jacksonville",
+    team: "Jacksonville Jaguars",
+    index: 5.879657546,
+    rank: 29,
+    gradient: [{
+        offset: "70%",
+        color: "#006778"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-94.5783, 39.0997],
+    targetLocation: [13.3833, 52.5167],
+    city: "Kansas City",
+    team: "Kansas City Chiefs",
+    index: 18.34664125,
+    rank: 24,
+    gradient: [{
+        offset: "40%",
+        color: "#E31837"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+},  {
+    sourceLocation: [-90.0667, 29.9500],
+    targetLocation: [13.3833, 52.5167],
+    city:"New Orleans",
+    team: "New Orleans Saints",
+    index: 31.25533083,
+    rank: 14,
+    gradient: [{
+        offset: "40%",
+        color: "#9F8958"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-74.0059, 40.7127],
+    targetLocation: [13.3833, 52.5167],
+    city: "New York",
+    team: "New York Jets / Giants",
+    index: 21.12492739,
+    rank: 19,
+    gradient: [{
+        offset: "80%",
+        color: "#0B2265"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-75.1667, 39.9500],
+    targetLocation: [13.3833, 52.5167],
+    city: "Philadelphia",
+    team: "Philadelphia Eagles",
+    index: 19.68986602,
+    rank: 21,
+    gradient: [{
+        offset: "60%",
+        color: "#004953"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-79.9764, 40.4397],
+    targetLocation: [13.3833, 52.5167],
+    city: "Pittsburgh",
+    team: "Pittsburgh Steelers",
+    index: 33.95279492,
+    rank: 12,
+    gradient: [{
+        offset: "80%",
+        color: "#000000"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-117.1625, 32.7150],
+    targetLocation: [13.3833, 52.5167],
+    city: "San Diego",
+    team: "San Diego Chargers",
+    index: 21.64575865,
+    rank: 18,
+    gradient: [{
+        offset: "30%",
+        color: "#0073CF"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-90.1978, 38.6272],
+    targetLocation: [13.3833, 52.5167],
+    city: "St. Louis",
+    team: "St. Louis Rams",
+    index: 25.80098003,
+    rank: 17,
+    gradient: [{
+        offset: "60%",
+        color: "#002244"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-82.5458, 27.7625],
+    targetLocation: [13.3833, 52.5167],
+    city: "Tampa Bay",
+    team: "Tampa Bay Buccaneers",
+    index: 19.14344252,
+    rank: 23,
+    gradient: [{
+        offset: "50%",
+        color: "#D50A0A"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-86.7833, 36.1667],
+    targetLocation: [13.3833, 52.5167],
+    city: "Nashville",
+    team: "Tennessee Titans",
+    index: 9.978870314,
+    rank: 28,
+    gradient: [{
+        offset: "40%",
+        color: "#002244"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-70.3064, 44.2056],
+    targetLocation: [13.3833, 52.5167],
+    city: "New England",
+    team: "New England Patriots",
+    index: 100,
+    rank: 1,
+    gradient: [{
+        offset: "60%",
+        color: "#002244"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-77.0164, 38.9047],
+    targetLocation: [13.3833, 52.5167],
+    city: "Washington, D.C.",
+    team: "Washington Redskins",
+    index: 92.54370213,
+    rank: 2,
+    gradient: [{
+        offset: "40%",
+        color: "#773141"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-122.3331, 47.6097],
+    targetLocation: [13.3833, 52.5167],
+    city: "Seattle",
+    team: "Seattle Seahawks",
+    index: 82.16294604,
+    rank: 3,
+    gradient: [{
+        offset: "10%",
+        color: "#69BE28"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-104.9903, 39.7392],
+    targetLocation: [13.3833, 52.5167],
+    city: "Denver",
+    team: "Denver Broncos",
+    index: 81.83917531,
+    rank: 4,
+    gradient: [{
+        offset: "30%",
+        color: "orange"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-84.39, 33.7550],
+    targetLocation: [13.3833, 52.5167],
+    city: "Atlanta",
+    team: "Atlanta Falcons",
+    index: 81.34448,
+    rank: 5,
+    gradient: [{
+     offset: "40%",
+        color: "#A71930"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-76.6167, 39.2833],
+    targetLocation: [13.3833, 52.5167],
+    city: "Baltimore",
+    team: "Baltimore Ravens",
+    index: 78.13104315,
+    rank: 6,
+    gradient: [{
+        offset: "40%",
+        color: "#241773"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+
+}, {
+    sourceLocation: [-93.2650, 44.9778],
+    targetLocation: [13.3833, 52.5167],
+    city: "Minneapolis",
+    team: "Minnesota Vikings",
+    index: 69.87600084,
+    rank: 7,
+    gradient: [{
+        offset: "40%",
+        color: "#4F2683"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-80.8433, 35.2269],
+    targetLocation: [13.3833, 52.5167],
+    city: "Charlotte",
+    team: "Carolina Panthers",
+    index: 51.19283083,
+    rank: 8,
+    gradient: [{
+        offset: "70%",
+        color: "#0085CA"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-84.5167, 39.1],
+    targetLocation: [13.3833, 52.5167],
+    city: "Cincinnati",
+    team: "Cincinnati Bengals",
+    index: 42.76668715,
+    rank: 9,
+    gradient: [{
+        offset: "40%",
+        color: "#FB4F14"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}, {
+    sourceLocation: [-80.2089, 25.7753],
+    targetLocation: [13.3833, 52.5167],
+    city: "Miami",
+    team: "Miami Dolphins",
+    index: 38.55842793,
+    rank: 10,
+    gradient: [{
+        offset: "40%",
+        color: "#008E97"
+    }],
+    byTime:[{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"},{time: "1", color:"grey"}]
+}]
